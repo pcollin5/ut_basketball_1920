2ca21cd4f29468b2cc00452e268aa67c44452888
Paul Collins <pncollins5@gmail.com>
2019-11-06 15:03:40 -0500

    commit

---
+++ season_script.R
@@ -0,0 +1,10 @@
+###season_total_stuff
+
+ut_game_ids <- get_game_ids("Tennessee")
+
+unca_season_to_date <- assist_net(team = "UNC Asheville", node_col = "Blue", season = "2019-20")
+
+ut_season_to_date11062019 <- assist_net(team = "Tennessee", node_col = ut_color_primary$primary_color, season = "2019-20")
+
+
+##next up is murray sate, they play saturday###
